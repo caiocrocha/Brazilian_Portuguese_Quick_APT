2961ee1b273ed9f64e3cd6b4e009086e959d0042
Caio Rocha <caio.cedrola@estudante.ufjf.br>
2024-03-29 12:48:17 -0300

    Add espeak-ng and analysis of cer

--- run_g2ps.sh
+++ run_g2ps.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 # Input and output file paths
-input_file="pronunciations/pronunciation_sp_val.txt"
-output_file="pronunciations/pronunciation_sp_val"
+input_file="$1"
+output_dir="$2"
+output_prefix="$3"
 
-cat "$input_file" | docker run --rm -i falabrasil/g2p | awk '{print $1 "," substr($0, length($1) + 2)}' > "${output_file}_falabrasil.csv"
+if [ $# -lt 3 ]; then
+	echo "Missing arguments"
+	echo "1: input text file"
+	echo "2: output directory"
+	echo "3: prefix of output files, which will be saved as OUTPUT_DIR/PREFIX_G2P.csv"
+	exit
+fi
 
+mkdir -p "$output_dir"
 
-> "${output_file}_espeak.csv"
+echo "Falabrasil G2P"
+cat "$input_file" | docker run --rm -i falabrasil/g2p | awk '{print $1 "," substr($0, length($1) + 2)}' > "${output_dir}/${output_prefix}_falabrasil.csv"
 
-# Loop through each line in the input file
-while IFS=, read -r word
-do
-    # Use espeak to convert the word to phonemes
-    phonemes=$(echo "$word" | espeak --stdin -v pt -q -x --ipa=3 | sed -e '/^$/d' -e 's/ː//g' -e 's/ˈ//g' -e 's/ˌ//g' -e 's/\n//g')
+echo "espeak G2P"
+espeak -f $input_file -v pt -q -x --ipa | sed -e 's/[ːˈˌ]//g' -e 's/  */\n/g' | grep "\S" > "${output_dir}/${output_prefix}_espeak.txt" && paste -d, $input_file "${output_dir}/${output_prefix}_espeak.txt" > "${output_dir}/${output_prefix}_espeak.csv"
 
-    # Append the word ID, word, and phonemes to the output file
-    echo "$word,$phonemes" >> "${output_file}_espeak.csv"
-done < "$input_file"
+echo "espeak-ng G2P"
+espeak-ng -f $input_file -v pt-br -q -x --ipa | sed -e 's/[ːˈˌ]//g' -e 's/  */\n/g' | grep "\S" > "${output_dir}/${output_prefix}_espeak-ng.txt" && paste -d, $input_file "${output_dir}/${output_prefix}_espeak-ng.txt" > "${output_dir}/${output_prefix}_espeak-ng.csv"
 
+echo "Phonetisaurus G2P"
 phonetisaurus_model_path="/home/caio/Documents/github/g2ps/models/portuguese.fst"
-
-phonetisaurus-g2pfst --model="$phonetisaurus_model_path" --wordlist="$input_file" | awk '{print $1 "," substr($0, length($1 $2) + 3)}' > "${output_file}_phonetisaurus.csv"
+phonetisaurus-g2pfst --model="$phonetisaurus_model_path" --wordlist="$input_file" | awk '{print $1 "," substr($0, length($1 $2) + 3)}' > "${output_dir}/${output_prefix}_phonetisaurus.csv"
